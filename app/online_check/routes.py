2ebb38b628e78f25b9aca83ce030a9ca645fe117
akurm <kurm_de@kurm.de>
2019-04-19 20:18:58 +0200

    Some changes after merging

--- app/online_check/routes.py
+++ app/online_check/routes.py
@@ -1,18 +1,13 @@
 
 from app.online_check import bp
-from app.models import User
-from flask_login import current_user, login_user, logout_user
-from app.models import User, Role
-from werkzeug.urls import url_parse
-from flask_login import login_required
-import json
-from flask import render_template, redirect, flash
-from app.onlinecheck.forms import NewOnlineCheckForm
+from flask import render_template, redirect, flash, url_for
+from app.online_check.forms import NewOnlineCheckForm
 
 @bp.route('/start_new_online_check', methods=['GET', 'POST'])
 def start_new_online_check():
     form = NewOnlineCheckForm()
-    #if form.validate_on_submit():
-        #pass
-    return render_template('online_check/new_online_check_form.html', title='1Online Check erstellen', form=form)
+    if form.validate_on_submit():
+        flash('Neuer Online Check wurde erstellt.', 'success')
+        return redirect(url_for('main.index'))
+    return render_template('online_check/new_online_check_form.html', title='Online Check erstellen', form=form)
 
