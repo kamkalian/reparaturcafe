52639e7b88ae961eb36356e6a3af436d7f9221f3
akurm <kurm_de@kurm.de>
2019-05-09 13:53:13 +0200

    Added FFPingCheck

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,6 @@ bootstrap = Bootstrap()
 login = LoginManager()
 mail = Mail()
 
-
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,9 @@
 from app.main import bp
 from flask import render_template, flash, redirect
 from app.forms import LoginForm
+from app.models import FFPing
+from app import db
+import json
 
 
 @bp.route('/')
@@ -18,3 +21,19 @@ def login():
         flash('Du bist nun eingeloggt.', 'success')
         return redirect('/index')
     return render_template('auth/login.html', title='Login', form=form)
+
+
+@bp.route('/ff_router_stat')
+def ff_router_stat():
+
+    return redirect('/ff_router_stat')
+
+
+@bp.route('/ff_router_sign_of_life/<response_time>')
+def ff_router_sign_of_life(response_time):
+    ffping = FFPing(response_time=response_time)
+    db.session.add(ffping)
+    db.session.commit()
+    return '''True'''
+
+
