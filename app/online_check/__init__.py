2bd70cb30a7b06c76bfb5db0f2b321ee7186d932
Andreas Kurm <andreas.kurm@siegwerk.com>
2019-12-29 09:26:04 +0100

    Added attachment import

--- app/online_check/__init__.py
+++ app/online_check/__init__.py
@@ -9,6 +9,6 @@ Created on Fri Aug 24 13:51:58 2018
 from flask import Blueprint
 bp = Blueprint('online_check', __name__)
 
-from app.online_check import routes
+from app.online_check import routes, attachments
 
 
