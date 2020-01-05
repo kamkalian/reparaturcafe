68298e01b6944fcb453902213a4277abb6c110fd
Andreas Kurm <andreas.kurm@siegwerk.com>
2020-01-05 20:02:01 +0100

    Changed return of attachment_list to a json list

--- app/online_check/attachments.py
+++ app/online_check/attachments.py
@@ -1,15 +1,25 @@
 from app.online_check import bp
 from flask_user import current_user, login_required
+from flask import request
 from app.models import Attachment
+import json
 
 
-@bp.route('/attachment_list/oc_id', methods=['POST'])
+@bp.route('/attachment_list', methods=['POST'])
 @login_required
-def attachment_list(oc_id):
+def attachment_list():
     '''
-    Lädt alle Anhänge zu einem Onlinecheck und gibt sie als Liste zurück.
+    Lädt alle Anhänge zu einem Onlinecheck und gibt sie als Json Liste zurück.
     '''
+
+    # ID aus den übermittelten POST Daten holen
+    oc_id = request.form.get('oc_id')
+    print('##############')
+    print(oc_id)
 
     attachment_list = Attachment.query.filter_by(online_check_id=oc_id).all()
+    attachment_filename_list = []
+    for attachment in attachment_list:
+        attachment_filename_list.append(attachment.filename)
 
-    return attachment_list
+    return json.dumps(attachment_filename_list)
