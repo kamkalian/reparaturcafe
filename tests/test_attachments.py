1fa644156ecd4da1ccbf7ba4651a8fbff8e822e5
Andreas Kurm <andreas.kurm@siegwerk.com>
2020-01-05 20:04:10 +0100

    Adjusted tests for attachment, but current it is not yet working

--- tests/test_attachments.py
+++ tests/test_attachments.py
@@ -1,15 +1,19 @@
 import pytest
 from app.online_check import attachments
+import json
 
 
 @pytest.mark.parametrize(
     "oc_id", [1, 2]
 )
-def test_attachment_list(app, auth, oc_id):
+def test_attachment_list(app, auth, client, oc_id):
  
     auth.login()
 
-    attachment_list = attachments.attachment_list(oc_id)
+    data = {'oc_id':oc_id}
+    attachment_list_json = client.post('/attachment_list', data=json.dumps(data))
+    print(attachment_list_json.json)
+    attachment_list = json.loads(attachment_list_json.json)    
 
     assert len(attachment_list) > 0
 
@@ -21,6 +25,7 @@ def test_empty_attachment_list(auth, oc_id):
 
     auth.login() 
     
-    attachment_list = attachments.attachment_list(oc_id)
+    attachment_list_json = attachments.attachment_list(oc_id)
+    attachment_list = json.loads(attachment_list_json)
 
     assert len(attachment_list) == 0
