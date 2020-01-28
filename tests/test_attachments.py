e40fd0ec5a92cb5c42b39223afe8191d4af458fb
Andreas Kurm <andreas.kurm@siegwerk.com>
2020-01-28 20:22:13 +0100

    Added tests for attachment upload

--- tests/test_attachments.py
+++ tests/test_attachments.py
@@ -29,3 +29,35 @@ def test_empty_attachment_list(auth, client, oc_id):
     attachment_list = json.loads(attachment_list_json.data)
 
     assert len(attachment_list) == 0
+
+
+@pytest.mark.parametrize(
+    "filename", ['test1.png', 'test2.jpg', 'test3.pdf']
+)
+def test_allowed_extensions(auth, client, filename):
+    '''
+    Prüfen ob bestimmte Dateieendungen erlaubt sind.
+    '''
+
+    assert attachments.allowed_file(filename)
+
+
+@pytest.mark.parametrize(
+    "oc_id", [1, 2]
+)
+def test_attachment_upload(auth, client, oc_id):
+    '''
+    Prüft ob der Datei Upload funktioniert.
+    '''
+ 
+    # Testuser einloggen
+    auth.login()
+
+    # Prüfen ob der redirect funktioniert
+    assert client.post("/attachment_upload", data={'oc_id':oc_id}).status_code == 302
+
+    
+
+
+
+
